09b86f20fd935f3aabccbf9ec05a7997a38045e2
Lisa Gillfrost <smartalisa@hotmail.com>
2024-05-29 11:48:27 +0200

    add addNumber button

--- app/src/main/java/com/example/pia12androidintro/MainActivity.kt
+++ app/src/main/java/com/example/pia12androidintro/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var minSiffra = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,8 +25,21 @@ class MainActivity : AppCompatActivity() {
        var minText =  findViewById<TextView>(R.id.fancyText)
         minText.text = "Banan"
 
-        findViewById<Button>(R.id.button)
+        var minKnapp = findViewById<Button>(R.id.button)
+        minKnapp.setOnClickListener {
+            minText.text = "Apelsin"
+        }
 
+        var mittNummer =  findViewById<TextView>(R.id.showNumber)
+
+
+
+        var adderaSiffra = findViewById<Button>(R.id.addNumberButton)
+
+        adderaSiffra.setOnClickListener {
+            minSiffra = minSiffra + 1
+            mittNummer.text = minSiffra.toString()
+        }
 
     }
 }
